d09971a5d62da063003cf11d5c138054026e9d93
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2022-10-22 13:02:48 +0200

    project card hidden before animation active

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
+import { useState, useEffect } from "react";
 
 export const Projects = () => {
   const projects = [
@@ -25,8 +26,27 @@ export const Projects = () => {
       title: "Business Startup",
       description: "https://github.com/Marshan88/CSharpProjects",
       imgUrl: projImg3,
+      link: "https://github.com/Marshan88/CSharpProjects",
     },
   ];
+
+  const [isVisibles, setIsVisible] = useState(true);
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
+  const listenToScroll = () => {
+    let heightToHideFrom = 1000;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > heightToHideFrom) {
+      isVisibles &&
+        setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
 
   return (
     <section className="project" id="projects">
@@ -40,70 +60,71 @@ export const Projects = () => {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeInUp" : ""
-                  }
-                >
-                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    <Nav
-                      variant="pills"
-                      className="nav-pills mb-5 justify-content-center align-items-center"
-                      id="pills-tab"
-                    >
-                      <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                      </Nav.Item>
-                    </Nav>
-                    <Tab.Content
-                      id="slideInUp"
-                      className={
-                        isVisible ? "animate__animated animate__zoomIn" : ""
-                      }
-                    >
-                      <Tab.Pane eventKey="first">
-                        <Row>
-                          {projects.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
-                          })}
-                        </Row>
-                      </Tab.Pane>
-                      <Tab.Pane eventKey="second">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Cumque quam, quod neque provident velit, rem
-                          explicabo excepturi id illo molestiae blanditiis,
-                          eligendi dicta officiis asperiores delectus quasi
-                          inventore debitis quo.
-                        </p>
-                      </Tab.Pane>
-                      <Tab.Pane eventKey="third">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Cumque quam, quod neque provident velit, rem
-                          explicabo excepturi id illo molestiae blanditiis,
-                          eligendi dicta officiis asperiores delectus quasi
-                          inventore debitis quo.
-                        </p>
-                      </Tab.Pane>
-                    </Tab.Content>
-                  </Tab.Container>
-                </div>
-              )}
-            </TrackVisibility>
+            {isVisibles && (
+              <TrackVisibility id="reveal">
+                {({ isVisible }) => (
+                  <div
+                    className={
+                      isVisible ? "animate__animated animate__fadeInUp" : ""
+                    }
+                  >
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                      <Nav
+                        variant="pills"
+                        className="nav-pills mb-5 justify-content-center align-items-center"
+                        id="pills-tab"
+                      >
+                        <Nav.Item>
+                          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                      <Tab.Content
+                        id="slideInUp"
+                        className={
+                          isVisible ? "animate__animated animate__zoomIn" : ""
+                        }
+                      >
+                        <Tab.Pane eventKey="first">
+                          <Row>
+                            {projects.map((project, index) => {
+                              return <ProjectCard key={index} {...project} />;
+                            })}
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Cumque quam, quod neque provident velit, rem
+                            explicabo excepturi id illo molestiae blanditiis,
+                            eligendi dicta officiis asperiores delectus quasi
+                            inventore debitis quo.
+                          </p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Cumque quam, quod neque provident velit, rem
+                            explicabo excepturi id illo molestiae blanditiis,
+                            eligendi dicta officiis asperiores delectus quasi
+                            inventore debitis quo.
+                          </p>
+                        </Tab.Pane>
+                      </Tab.Content>
+                    </Tab.Container>
+                  </div>
+                )}
+              </TrackVisibility>
+            )}
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" alt="" src={colorSharp2}></img>
     </section>
   );
-  
 };
