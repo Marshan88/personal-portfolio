b1cf1fb7341b75e17fe4c9c97524cb90d302f355
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2023-04-25 15:00:34 +0200

    Todo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,17 +9,17 @@ import tetris from "../assets/img/tetris.png";
 import reactportfolio from "../assets/img/reactportfolio.png";
 import TrackVisibility from "react-on-screen";
 import "animate.css";
-
 /* 
   TODO:
+  - Convert to Next.js!
   - Make project cards clickable. (middle mouse button)
+  - Create new site for Gamedev projects, and one for drone projects and add links.
+  - Make it mobile friendly, swipe on project tab and onTouch on project cards.
+  - Make scroll and swipe animations go to next section.
 
   Color theme:
   - #FF00A8 pink, #FFDD19 yellow, #00FFA3 green, #2D2D2D dark gray, #2F2F2F light gray
 */
-
-
-// From projectcard template
 export const Projects = () => {
   const firstTabProjects = [
     {
@@ -81,29 +81,31 @@ export const Projects = () => {
       link: "https://marshan.me/#projects",
     },
   ];
+  // //swipe animation
+  // const [touchStart, setTouchStart] = useState(null)
+  // const [touchEnd, setTouchEnd] = useState(null)
 
-  //swipe animation
-  const [touchStart, setTouchStart] = useState(null)
-  const [touchEnd, setTouchEnd] = useState(null)
+  // // the required distance between touchStart and touchEnd to be detected as a swipe
+  // const minSwipeDistance = 50
 
-  // the required distance between touchStart and touchEnd to be detected as a swipe
-  const minSwipeDistance = 50
+  // const onTouchStart = (e) => {
+  //   setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+  //   setTouchStart(e.targetTouches[0].clientX)
+  // }
 
-  const onTouchStart = (e) => {
-    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
-    setTouchStart(e.targetTouches[0].clientX)
-  }
+  // const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
 
-  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+  // const onTouchEnd = () => {
+  //   if (!touchStart || !touchEnd) return
+  //   const distance = touchStart - touchEnd
+  //   const isLeftSwipe = distance > minSwipeDistance
+  //   const isRightSwipe = distance < -minSwipeDistance
+  //   if (isLeftSwipe || isRightSwipe) console.log('swipe', isLeftSwipe ? 'left' : 'right')
+  //   // add your conditional logic here
+  // }
 
-  const onTouchEnd = () => {
-    if (!touchStart || !touchEnd) return
-    const distance = touchStart - touchEnd
-    const isLeftSwipe = distance > minSwipeDistance
-    const isRightSwipe = distance < -minSwipeDistance
-    if (isLeftSwipe || isRightSwipe) console.log('swipe', isLeftSwipe ? 'left' : 'right')
-    // add your conditional logic here
-  }
+
+  // <div>onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}</div>
 
   return (
     <section className="project" id="projects">
@@ -118,7 +120,7 @@ export const Projects = () => {
                   <br></br><br></br>
                 </div>}
             </TrackVisibility>
-            <Tab.Container id="projects-tabs" defaultActiveKey="first" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
               <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                 <Nav.Item className="hoverEffect">
                   <Nav.Link eventKey="first">Front-end</Nav.Link>
