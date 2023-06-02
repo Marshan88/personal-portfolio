816fbd754f1b2ed913fe4dc3c1a93556c3cff4d0
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2023-06-02 13:01:53 +0200

    udpate
    banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
-import Typed from 'typed.js';
+import Typed from "typed.js";
 import logo from "../assets/img/logo.png";
 
 /*
   TODO:
   * (A) Make it more personal, less professional. People want to hire a friend, more than they want to hire someone productive.
   * (A) Improve hexagon background.
+  * (A) Convert to NextJs.
   * (A) Make it mobile friendly, swipe on project tab and onTouch on project cards.
-  * (B) Make project cards clickable. (middle mouse button)
   * (B) Make scroll and swipe animations go to next section.
   * (B) Make a new section for Gamedev projects.
-  * (B) Convert to Next.js!
   * (C) Make a new section for other projects.
+  * (C) Make project cards clickable. (middle mouse button)
 
   Color theme:
   - #FF00A8 pink, #FFDD19 yellow, #00FFA3 green, #2D2D2D dark gray, #2F2F2F light gray
@@ -26,7 +26,7 @@ export const Banner = () => {
   const el = React.useRef(null);
   React.useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['<i>Front</i>-end.', '<i>Back</i>-end.', '<i>Full</i>-stack.'],
+      strings: ["<i>Front</i>-end.", "<i>Back</i>-end.", "<i>Full</i>-stack."],
       typeSpeed: 60,
       backSpeed: 35,
       backDelay: 2000,
@@ -38,23 +38,36 @@ export const Banner = () => {
     };
   }, []);
 
-
-
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline" style={{ userSelect: "none" }}>Welcome to my Portfolio</span>
+            <span className="tagline" style={{ userSelect: "none" }}>
+              Welcome to my Portfolio
+            </span>
             <h2>{`Designing websites / apps for companies`}</h2>
             <h2>
               <span ref={el} />
             </h2>
             <br></br>
             <p>
-              I'm Marius, I like programming, tech and online competative games.<br></br>
-              Did a course in front-end and back-end at <a className="banner-a" href="https://www.getacademy.no/" target="_blank" rel="noopener noreferrer">GET-Academy</a>,<br></br>
-              currently looking for work/internship.<br></br><br></br></p>
+              I'm Marius, I like programming, tech and online competative games.
+              <br></br>
+              Did a course at{" "}
+              <a
+                className="banner-a"
+                href="https://www.getacademy.no/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GET-Academy
+              </a>
+              ,<br></br>
+              where i learned the basics for front and back-end.<br></br>
+              currently looking for work/internship.<br></br>
+              <br></br>
+            </p>
             <p className="banner-skills-p">Skills:</p>
             <ul>
               <li>HTML, CSS, JavaScript</li>
@@ -66,14 +79,24 @@ export const Banner = () => {
           </Col>
           <Col>
             <TrackVisibility once partialVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomInDown" : ""}>
-                  <img src={logo} className="logo" alt="Logo" style={{ userSelect: "none" }} />
-                </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomInDown" : ""
+                  }
+                >
+                  <img
+                    src={logo}
+                    className="logo"
+                    alt="Logo"
+                    style={{ userSelect: "none" }}
+                  />
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-    </section >
-  )
-}
+    </section>
+  );
+};
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 import { Tilt } from "react-tilt";
 
 const handleClick = (url) => () => {
-  var a = document.createElement('a');
+  var a = document.createElement("a");
   a.target = "_blank";
   a.rel = "noopener noreferrer";
   a.href = url;
@@ -12,27 +12,27 @@ const handleClick = (url) => () => {
 };
 
 const defaultOptions = {
-  reverse: true,                              // reverse the tilt direction
-  max: 14,                                    // max tilt rotation (degrees)
-  perspective: 2000,                          // Transform perspective, the lower the more extreme the tilt gets.
-  scale: 1,                                   // 2 = 200%, 1.5 = 150%, etc..
-  speed: 450,                                 // Speed of the enter/exit transition
-  transition: true,                           // Set a transition on enter/exit.
-  axis: null,                                 // What axis should be disabled. Can be X or Y.
-  reset: true,                                // If the tilt effect has to be reset on exit.
-  easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  reverse: true, // reverse the tilt direction
+  max: 14, // max tilt rotation (degrees)
+  perspective: 2000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 450, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
 };
 // target="_blank"
 export const ProjectCard = ({ title, description, imgUrl, url }) => {
   return (
     <Col onClick={handleClick(url)}>
-      <Tilt options={defaultOptions} className="proj-imgbx" >
+      <Tilt options={defaultOptions} className="proj-imgbx">
         <img src={imgUrl} alt="Projectimage" />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
       </Tilt>
-    </Col >
+    </Col>
   );
 };
